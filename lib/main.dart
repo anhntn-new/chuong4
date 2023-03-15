eb2363f1d6b9b208ce9ec52542061d625d4f30d4
anhntn-new <nguyetanh150299@gmail.com>
2023-03-15 18:10:00 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,301 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20,
+                vertical: 10,
+              ),
+              child: Column(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 20,
+                      vertical: 12,
+                    ),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      color: Colors.white38,
+                      border: Border.all(
+                        color: Colors.black26,
+                        width: 2,
+                      ),
+                    ),
+                    child: Row(
+                      children: [
+                        Image.asset(
+                          'assets/imgs/FAQ.png',
+                          height: 28,
+                          fit: BoxFit.fill,
+                        ),
+                        const SizedBox(width: 20),
+                        const Text(
+                          'FAQ',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w700,
+                            color: Colors.black38,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 18),
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 20,
+                      vertical: 12,
+                    ),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      color: Colors.white38,
+                      border: Border.all(
+                        color: Colors.black26,
+                        width: 2,
+                      ),
+                    ),
+                    child: Row(
+                      children: [
+                        Image.asset(
+                          'assets/imgs/Group.png',
+                          height: 28,
+                          fit: BoxFit.fill,
+                        ),
+                        const SizedBox(width: 20),
+                        const Text(
+                          'Contact Us',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w700,
+                            color: Colors.black38,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 18),
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 20,
+                      vertical: 12,
+                    ),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      color: Colors.white38,
+                      border: Border.all(
+                        color: Colors.black26,
+                        width: 2,
+                      ),
+                    ),
+                    child: Row(
+                      children: [
+                        Image.asset(
+                          'assets/imgs/terms.png',
+                          height: 28,
+                          fit: BoxFit.fill,
+                        ),
+                        const SizedBox(width: 20),
+                        const Text(
+                          'Term & Conditions',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w700,
+                            color: Colors.black38,
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            const Divider(
+              color: Colors.red,
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20,
+                vertical: 10,
+              ),
+              child: Row(
+                children: [
+                  const CircleAvatar(
+                    backgroundImage: NetworkImage(
+                      'https://blog.logrocket.com/wp-content/uploads/2021/04/Building-Flutter-desktop-app-tutorial-examples.png',
+                    ),
+                    radius: 30,
+                  ),
+                  const SizedBox(width: 20),
+                  Column(
+                    // mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Text(
+                        '@sakeshioyaki15',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                      Text(
+                        'Nguyet Anh',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w400,
+                          fontSize: 14,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            const Divider(
+              color: Colors.red,
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20,
+                vertical: 10,
+              ),
+              child: Column(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 20,
+                      vertical: 12,
+                    ),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      color: Colors.white38,
+                      border: Border.all(
+                        color: Colors.black26,
+                        width: 2,
+                      ),
+                    ),
+                    child: Row(
+                      children: [
+                        SvgPicture.asset(
+                          'assets/svgs/FAQ.svg',
+                          height: 28,
+                          fit: BoxFit.fill,
+                        ),
+                        const SizedBox(width: 20),
+                        const Text(
+                          'FAQ',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w700,
+                            color: Colors.black38,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 18),
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 20,
+                      vertical: 12,
+                    ),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      color: Colors.white38,
+                      border: Border.all(
+                        color: Colors.black26,
+                        width: 2,
+                      ),
+                    ),
+                    child: Row(
+                      children: [
+                        SvgPicture.asset(
+                          'assets/svgs/Contact.svg',
+                          height: 28,
+                          fit: BoxFit.fill,
+                        ),
+                        const SizedBox(width: 20),
+                        const Text(
+                          'Contact Us',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w700,
+                            color: Colors.black38,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 18),
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 20,
+                      vertical: 12,
+                    ),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      color: Colors.white38,
+                      border: Border.all(
+                        color: Colors.black26,
+                        width: 2,
+                      ),
+                    ),
+                    child: Row(
+                      children: [
+                        SvgPicture.asset(
+                          'assets/svgs/terms.svg',
+                          height: 28,
+                          fit: BoxFit.fill,
+                        ),
+                        const SizedBox(width: 20),
+                        const Text(
+                          'Term & Conditions',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w700,
+                            color: Colors.black38,
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            const Divider(
+              color: Colors.red,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
