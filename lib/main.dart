37c2cdaeae9db1f83ae1df97d49532f4b0249997
anhntn-new <anhntn@newwave.vn>
2023-04-09 14:41:37 +0700

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:chuong4/unit_5/config/custom_router.dart';
 import 'package:chuong4/unit_5/config/route_paths.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,8 +11,6 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
-
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
